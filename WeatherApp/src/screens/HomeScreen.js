48de09b947cceabb9c89e1b04be8a88e99fb43fb
Vitor de Carvalho Silva <vcarvalho.coantact@gmail.com>
2025-03-07 11:02:21 -0300

    Inicialização do projeto

---
+++ WeatherApp/src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
+
+export default function HomeScreen() {
+  return (
+    <LinearGradient
+        colors={['rgba(173,185,212,1)', 'rgba(0,30,94,1)']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        style={styles.container}>
+        
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+});
